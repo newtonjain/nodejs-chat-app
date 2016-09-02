22881668f8f98076c48131d012dbb12d28d37cd8
Newton Jain <newton.jain@gmail.com>
2016-09-01 17:05:11 -0700

    adding glimpse back

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ io.sockets.on('connection', (socket) => {
   });
 });
 
-server.llisten(process.env.PORT || 5000, function(){
+server.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
